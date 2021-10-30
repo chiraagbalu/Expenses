958f08a6d9187864ff171977d171fc7d0d7e47b7
chiraagbalu <chiraagbalu@berkeley.edu>
2021-10-29 19:22:32 -0700

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Chiraag Balu on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
