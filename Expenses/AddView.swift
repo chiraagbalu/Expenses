005852e0f23cbf64faba4488ec624b72a8314f88
chiraagbalu <chiraagbalu@berkeley.edu>
2021-10-30 20:23:52 -0700

    Created Expenses App Rough Draft (no documentation w/ comments yet) (could be prettier)

---
+++ Expenses/AddView.swift
@@ -0,0 +1,52 @@
+//
+//  AddView.swift
+//  Expenses
+//
+//  Created by Chiraag Balu on 10/29/21.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "personal"
+    @State private var amount = ""
+    @State private var showingAlert = false
+    
+    static let types = ["business", "personal"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        if let actualAmount = Int(self.amount) {
+                            let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                            self.expenses.items.append(item)
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+            })
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
--- Expenses/ContentView.swift
+++ Expenses/ContentView.swift
@@ -7,10 +7,103 @@
 
 import SwiftUI
 
+
+//creates struct for expense item type
+struct ExpenseItem : Identifiable, Codable {
+    //allows for dynamic ID creation and assignment
+    let id = UUID()
+    //each expense has a name, type of expense, and amount
+    let name : String
+    let type: String
+    let amount : Int
+}
+
+//class for expenses list
+class Expenses: ObservableObject {
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        self.items = []
+    }
+    
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    @State private var listRowColor = Color.white
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List {
+                ForEach(expenses.items) { item in
+                    if(item.type=="business") {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            Spacer()
+                            Text("$\(item.amount)")
+                        }
+                        .padding(10)
+                        .background(RoundedRectangle(cornerRadius:5).foregroundColor(Color(red: 0.5, green: 0.7, blue: 0.9)))
+
+                    } else {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            Spacer()
+                            Text("$\(item.amount)")
+                        }
+                        .padding(10)
+                        .background(RoundedRectangle(cornerRadius:5).foregroundColor(Color(red: 0.99, green: 0.5, blue: 0.5)))
+                        
+                    }
+                    
+                }
+                .onDelete(perform: removeItems)
+                .listRowBackground(listRowColor)
+
+            }
+            .navigationBarTitle("Expenses")
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        self.showingAddExpense = true
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                }
+                
+            })
+            
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: self.expenses)
+        }
+    }
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
